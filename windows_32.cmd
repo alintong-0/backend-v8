71011638f4fd3e916e3e9c42f04234786e872107
alintong-0 <62975670+alintong-0@users.noreply.github.com>
2024-01-22 12:18:04 +0800

    Update windows_32.cmd

--- windows_32.cmd
+++ windows_32.cmd
@@ -18,12 +18,12 @@ set inputFile2="./depot_tools/metrics_utils.py"
 set searchString="https://chromium.googlesource.com/v8/v8.git"
 set replaceString="https://github.com/alintong-0/v8.git"
 
-set searchString2="https://chromium.googlesource.com/v8/v8"
-set replaceString2="https://github.com/alintong-0/v8"
+set searchString2="KNOWN_PROJECT_URLS = {"
+set replaceString2="KNOWN_PROJECT_URLS = {https://github.com/alintong-0/v8,"
 
 rem 用 powershell 命令读取文件并替换字符串
 powershell -Command "(Get-Content %inputFile%) -replace '%searchString%', '%replaceString%' | Set-Content %inputFile%"
-powershell -Command "(Get-Content %inputFile2%) -replace '%searchString%', '%replaceString%' | Set-Content %inputFile2%"
+powershell -Command "(Get-Content %inputFile2%) -replace '%searchString2%', '%replaceString2%' | Set-Content %inputFile2%"
 
 endlocal
 
