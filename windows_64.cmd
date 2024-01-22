1e4a79c2266121596ef8fd4a616bab764ca1927a
alintong-0 <62975670+alintong-0@users.noreply.github.com>
2024-01-22 16:54:36 +0800

    test

--- windows_64.cmd
+++ windows_64.cmd
@@ -19,21 +19,11 @@ echo =====[ Reset V8 Git ]=====
 setlocal enabledelayedexpansion
 
 set inputFile="./depot_tools/fetch_configs/v8.py"
-set inputFile2="./depot_tools/metrics_utils.py"
-set inputFile3="./depot_tools/recipes/recipe_modules/bot_update/examples/full.py"
-
 set searchString="https://chromium.googlesource.com/v8/v8.git"
-set searchString3="https://chromium.googlesource.com/v8/v8"
 set replaceString="https://github.com/alintong-0/v8.git"
-set replaceString3="https://github.com/alintong-0/v8"
-
-set searchString2="KNOWN_PROJECT_URLS = {"
-set replaceString2="KNOWN_PROJECT_URLS = {\"https://github.com/alintong-0/v8\","
 
 rem 用 powershell 命令读取文件并替换字符串
 powershell -Command "(Get-Content %inputFile%) -replace '%searchString%', '%replaceString%' | Set-Content %inputFile%"
-@REM powershell -Command "(Get-Content %inputFile2%) -replace '%searchString2%', '%replaceString2%' | Set-Content %inputFile2%"
-@REM powershell -Command "(Get-Content %inputFile3%) -replace '%searchString3%', '%replaceString3%' | Set-Content %inputFile3%"
 
 endlocal
 
