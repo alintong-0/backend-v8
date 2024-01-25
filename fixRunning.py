7566a5479f4cefd3e3ac029af6bb1d08b110bfe5
alintong-0 <62975670+alintong-0@users.noreply.github.com>
2024-01-25 12:10:04 +0800

    Update fixRunning.py

--- fixRunning.py
+++ fixRunning.py
@@ -1,6 +1,7 @@
 # Open the file and read its content
 file_path = "./v8/v8/build/toolchain/win/tool_wrapper.py"  # Replace with the path to the file you want to work with
 search_string = "if (not line.startswith('   Creating library ') and"
+search_string2 = "if not line.startswith(' Assembling: '):"
 new_line = "line = line.decode('utf8')"
 
 with open(file_path, "r") as file:
@@ -14,6 +15,10 @@ with open(file_path, "w") as file:
         if search_string in line:
             found = True
             file.write(line.replace(search_string,new_line) + "\n")  # Write the new line after it
+            file.write(line)  # Write the line containing the search string
+        elif search_string2 in line:
+            found = True
+            file.write(line.replace(search_string2,new_line) + "\n")  # Write the new line after it
             file.write(line)  # Write the line containing the search string
         else:
             file.write(line)  # Write the original line
