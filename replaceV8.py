4fdf6d18b47586635aed46ae07319ae13e772634
alintong-0 <62975670+alintong-0@users.noreply.github.com>
2024-02-19 20:52:23 +0800

    fix path

--- replaceV8.py
+++ replaceV8.py
@@ -6,9 +6,8 @@ destination_directory = "./v8/v8/src/"
 
 replaceFileList = [
     "ast/ast.h",
-    "src/d8.h",
-    "include/v8.h",
-    "src/d8.cc",
+    "d8/d8.h",
+    "d8/d8.cc",
 ]
 
 for file_path in replaceFileList:
